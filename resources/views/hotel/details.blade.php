45420af12eeecfcbc9b05ee7e874e1d51efb6190
methotoy <emeritobucogii@gmail.com>
2017-10-16 14:02:43 +0800

    Hotel.

--- resources/views/hotel/details.blade.php
+++ resources/views/hotel/details.blade.php
@@ -12,7 +12,7 @@
 						<div class="col-sm-8">
 							<h1>
 								Kiwiana Panorama
-								<br/>
+								<br>
 								<small>Sample Information</small>
 							</h1>
 							<div class="clearfix"></div>
@@ -22,12 +22,78 @@
 						</div>
 					</div>
 					<div class="row">
-						<span class="tag-icon">
-							<img src="/icons/adsl.png" class="tips">
-						</span>
+						<div class="col-sm-12">
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+							<span class="tag-icon">
+								<img src="/icons/adsl.png" class="tips">
+							</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<img src="/img/hotel.jpg" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<h5>Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details Sample Details </h5>
+						</div>
 					</div>
 				</div>
 				<div class="col-md-4">
+					<div class="col-sm-12 right-container">
+						<div class="boxed">
+							<h4>Kiwiana Panorama</h4>
+							<p>
+								<i class="fa fa-map-marker"></i>
+								Hotel Address
+								<br>
+								<i class="fa fa-phone"></i>
+								Hotel Phone
+								<br>
+								<i class="fa fa-envelope"></i>
+								Hotel Email
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</article> 
